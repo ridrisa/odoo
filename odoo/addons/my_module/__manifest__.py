2038249afb58613a603a5d1d434221fda4cf1091
ridrisa <138629783+ridrisa@users.noreply.github.com>
2024-07-20 19:32:36 +0300

    Create manifest file

---
+++ odoo/addons/my_module/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'My Module',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'views/actions.xml',
+        'views/menus.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True
+}
