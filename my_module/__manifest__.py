65b28e7839e1570b3a1feb415982a7ba1f29d43a
ridrisa <138629783+ridrisa@users.noreply.github.com>
2024-07-20 18:02:45 +0300

    Adding __manifest__.py

---
+++ my_module/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'My Module', 'version': '1.0', 'category': 'Custom',
+   'summary': 'Custom Module for Vehicles, Couriers, and Shipments',
+   'description': " ""
+    This module manages vehicles, couriers, and shipments."
+   ",
+   'author': 'Your Name',
+    'depends': ['base', 'sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/vehicle_views.xml',
+        'views/courier_views.xml',
+        'views/shipment_views.xml',
+        'views/sale_order_views.xml',
+        'views/menus.xml'
+    ],
+    'installable': True,
+    'application': True
+}
